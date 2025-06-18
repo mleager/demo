978b126c263b4c8753ecb5b9572c1e348ca05ad2
Mark Leager <106631893+mleager@users.noreply.github.com>
2025-06-17 21:16:34 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    aws = { 
+      source = "hashicorp/aws"
+      version = 5.99.1
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1
+}
+
+data "aws_ami" "al2023" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.al2023.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
